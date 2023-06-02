ca9f6ec411cff2870c65f80d753784646b590d0f
svloc <svloc39@gmail.com>
2023-06-02 16:22:54 +0530

    added new changes

--- src/main/java/com/green/stitch/task/greenstitch/controller/LoginController.java
+++ src/main/java/com/green/stitch/task/greenstitch/controller/LoginController.java
@@ -1,10 +1,10 @@
 package com.green.stitch.task.greenstitch.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,18 +34,19 @@ public class LoginController {
     }
 
     @PostMapping("/signup")
-    public ResponseEntity<String> createUser(@RequestBody User user) {
-        if (userRepository.findByEmail(user.getEmail()) != null) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("Username already exists");
-        }
-        user.setPassword(passwordEncoder().encode(user.getPassword()));
-        userRepository.save(user);
-        return ResponseEntity.status(HttpStatus.CREATED).body("User registered successfully");
+    public User createUser(@RequestBody User user) {
+        // if (userRepository.findByEmail(user.getEmail()) != null) {
+        //     return ResponseEntity.status(HttpStatus.CONFLICT).body("Username already exists");
+        // }
+        // user.setPassword(passwordEncoder().encode(user.getPassword()));
+        // userRepository.save(user);
+        // return ResponseEntity.status(HttpStatus.CREATED).body("User registered successfully");
+        return userRepository.save(user);
     }
     
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder(){ 
-        return new BCryptPasswordEncoder(); 
-    }
+    // @Bean
+    // public BCryptPasswordEncoder passwordEncoder(){ 
+    //     return new BCryptPasswordEncoder(); 
+    // }
 
 }
